d5a5429fa32824aa702b89540bdbd08efbf1f74b
thomas0114 <thomas.k98114@gmail.com>
2023-06-15 08:23:41 -0400

    updated main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { GameSystemProvider } from './Game/Context/GameSystem.context'
-import { BirdProvider } from './Game/Context/Bird.context'
-import { ObstacleProvider } from './Game/Context/Obstacle.context'
-import { ScoreProvider } from './Game/Context/Score.context'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GameSystemProvider } from "./Game/Context/GameSystem.context";
+import { BirdProvider } from "./Game/Context/Bird.context";
+import { ObstacleProvider } from "./Game/Context/Obstacle.context";
+import { ScoreProvider } from "./Game/Context/Score.context";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GameSystemProvider>
       <ScoreProvider>
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         </BirdProvider>
       </ScoreProvider>
     </GameSystemProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
