04a9a9c0153f28875033c5f35cb8eae64325bc22
thomas0114 <thomas.k98114@gmail.com>
2023-11-01 10:45:02 -0400

    updaetd mongoDB url

--- server/config/key.js
+++ server/config/key.js
@@ -1,6 +1,5 @@
 module.exports = {
   // mongoURI: "mongodb://localhost:27017/flappybee", // local
-  mongoURI:
-    "mongodb+srv://thomas0114:PowKWOrKSRnlAxFt@flappybee-cluster0.6hhselh.mongodb.net/flappybee", // compass
+  mongoURI: process.env.REACT_APP_MONGODB_URL, // compass
   secretOrKey: "secret",
 };
