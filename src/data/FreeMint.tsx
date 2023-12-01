f5a4a508ff1b740775091558b41dffdbfa15727a
thomas0114 <thomas.k98114@gmail.com>
2023-12-01 11:56:56 -0500

    updated build file

--- src/data/FreeMint.tsx
+++ src/data/FreeMint.tsx
@@ -1,7 +1,7 @@
 export const textFreeMintRules = [
   "1. Visit flappybee.com",
   "2. Connect your wallet.",
-  "3. Ensure you have selected the BNB test network (for instructions on switching to BNB test, please refer to : https://medium.com/@gezimosmanii/a-simple-guide-to-adding-goerli-testnet-to-your-metamask-wallet-2023-step-by-step-guide-ce34b70e3125).",
+  "3. Ensure you have selected the BNB Test network (for instructions on switching to BNB Testnet, please refer to : https://medium.com/@gezimosmanii/a-simple-guide-to-adding-goerli-testnet-to-your-metamask-wallet-2023-step-by-step-guide-ce34b70e3125).",
   "4. Play the game and record your score on BNB Test network.",
   "5. Your score multiplied by 1% will determine your reward on the mainnet. For instance, if you score 100 $BEET on the BNB Test network, you will be eligible to claim 10 $BEET on the mainnet.",
   "6. Tokens will be claimable on mainnet once the FREE MINT session concludes.",
